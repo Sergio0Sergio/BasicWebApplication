ad5848d65736294cfc2d286cbce5f1db6b61a59a
Sergei Grimanov <tosergi@gmail.com>
2016-10-09 20:06:23 +0300

    compiler-plugin was added to pom.xml logger was added to Main.java

--- src/main/java/ru/habrahabr/sergiosergio/main/Main.java
+++ src/main/java/ru/habrahabr/sergiosergio/main/Main.java
@@ -5,10 +5,15 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import ru.habrahabr.sergiosergio.servlets.AllRequestsServlet;
 
+import java.util.logging.Logger;
+
+
 /**
  * Created by sgrimanov on 07.10.2016.
  */
 public class Main {
+
+    public static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
@@ -18,8 +23,8 @@ public class Main {
 
         Server server = new Server(8080);
         server.setHandler(context);
-
         server.start();
+        log.info("Server started");
         server.join();
     }
 }
