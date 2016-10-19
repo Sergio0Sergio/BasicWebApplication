b76bb377dc217ed65c681fff368a8f5aea6c8ea0
Sergei Grimanov <tosergi@gmail.com>
2016-10-19 18:31:19 +0300

    SignInServlet & SignUpServlet were added

---
+++ src/main/java/ru/habrahabr/sergiosergio/servlets/SignInServlet.java
@@ -0,0 +1,10 @@
+package ru.habrahabr.sergiosergio.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by sgrimanov on 19.10.2016.
+ */
+public class SignInServlet extends HttpServlet {
+
+}
---
+++ src/main/java/ru/habrahabr/sergiosergio/servlets/SignUpServlet.java
@@ -0,0 +1,33 @@
+package ru.habrahabr.sergiosergio.servlets;
+
+import ru.habrahabr.sergiosergio.accounts.AccountService;
+import ru.habrahabr.sergiosergio.accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * Created by sgrimanov on 19.10.2016.
+ */
+public class SignUpServlet extends HttpServlet {
+
+    private final AccountService accountService;
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        response.setContentType("text/html;charset=utf-8");
+
+        if (user == null){
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        } else {
+            accountService.addNewUser(new UserProfile(user));
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+}
