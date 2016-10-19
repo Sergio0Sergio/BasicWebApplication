70ac9474096fe356131f0187efa9e43997c0ebc1
Sergei Grimanov <tosergi@gmail.com>
2016-10-19 23:44:49 +0300

    n

--- src/main/java/ru/habrahabr/sergiosergio/servlets/SignInServlet.java
+++ src/main/java/ru/habrahabr/sergiosergio/servlets/SignInServlet.java
@@ -1,10 +1,15 @@
 package ru.habrahabr.sergiosergio.servlets;
 
+import ru.habrahabr.sergiosergio.accounts.AccountService;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by sgrimanov on 19.10.2016.
  */
 public class SignInServlet extends HttpServlet {
-
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    AccountService accountService = new AccountService();
 }
